bee0602769df3988476a91d1ebab23c12d6fdab9
unknown <garapatihariharasatyavishal@gmail.com>
2025-02-24 14:24:58 +0530

    My commit message

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -10,8 +10,6 @@ public class App {
         int b = scan.nextInt();
         int c = scan.nextInt();
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        System.out.printf("%d\n%d\n%d\n",a,b,c);
     }
 }
